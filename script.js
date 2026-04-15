ee851004be47f9a4bd1ddbf561390990a71fd519
sarakasai <hampom10@gmail.com>
2026-04-15 04:33:09 +0000

    I changed the topic of my create task, for I realized the wordle idea although is fun, is not the most practical to score well on the exam. Instead I decided on a simple gpa calculator, which will allow me to use arrays along with a function for calculating the grade. Today I redid my html for my new project idea, and integrated the grade dropdown menu along with the function to calculate the GPA.

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// add javascript here
+//list
+let grades = []; 
+
+//grade dropdown
+document.getElementById("add-btn").addEventListener("click", function() {
+    
+    let grade = document.getElementById("grade-dropdown").value;
+    grades.push(grade);
+    
+    let outputList = document.getElementById("grades-display");
+    outputList.innerHTML = outputList.innerHTML + "<li>" + grade + "</li>";
+});
+//calculate + display GPA
+document.getElementById("calc-btn").addEventListener("click", function() {
+    
+    let calculatedGPA = calcGPA(grades);
+    document.getElementById("gpa-result").innerHTML = calculatedGPA;
+});
